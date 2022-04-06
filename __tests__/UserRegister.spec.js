0d797bd2f4a19cc54309857d7c3364861e6e48c4
Masayuki <soy2.san@gmail.com>
2022-04-06 23:39:10 +0900

    databaseに値が入っているかテスト

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -1,5 +1,17 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+
+// sequelizeに繋げる
+beforeAll(() => {
+  return sequelize.sync();
+});
+
+// databaseの状態を毎回クリアする
+beforeEach(() => {
+  return User.destroy({ truncate: true });
+});
 
 // describeでテストをまとめる。クラスのようなもの
 describe('User Registration', () => {
@@ -30,6 +42,40 @@ describe('User Registration', () => {
       .then((response) => {
         expect(response.body.message).toBe('User created');
         done();
+      });
+  });
+
+  it('saves the user to database', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then(() => {
+        User.findAll().then((userList) => {
+          expect(userList.length).toBe(1);
+          done();
+        });
+      });
+  });
+
+  it('saves the username and email to database', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword',
+      })
+      .then(() => {
+        User.findAll().then((userList) => {
+          const savedUser = userList[0];
+          expect(savedUser.username).toBe('user1');
+          expect(savedUser.email).toBe('user1@mail.com');
+          done();
+        });
       });
   });
 });
