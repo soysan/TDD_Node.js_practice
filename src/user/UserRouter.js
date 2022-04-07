f48be7c0223c0aa8bb1013c6c3ea17729e515729
Masayuki <soy2.san@gmail.com>
2022-04-08 00:57:49 +0900

    add middleware test cases

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -3,10 +3,35 @@ const UserService = require('./UserService');
 
 const router = express.Router();
 
-router.post('/api/1.0/users', async (req, res) => {
+const validateUsername = (req, res, next) => {
+  const user = req.body;
+  if (user.username === null) {
+    req.validationErrors = {
+      username: 'Username cannot be null',
+    };
+  }
+  next();
+};
+
+const validateEmail = (req, res, next) => {
+  const user = req.body;
+  if (user.email === null) {
+    req.validationErrors = {
+      ...req.validationErrors,
+      email: 'Email cannot be null',
+    };
+  }
+  next();
+};
+
+// middleware (validateUsername)
+router.post('/api/1.0/users', validateUsername, validateEmail, async (req, res) => {
+  if (req.validationErrors) {
+    const response = { validationErrors: { ...req.validationErrors } };
+    return res.status(400).send(response);
+  }
   await UserService.save(req.body);
   return res.send({ message: 'User created' });
 });
 
 module.exports = router;
-
