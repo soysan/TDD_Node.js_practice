2da86ed2d519d9d6595c81363a41a75783439881
Masayuki <soy2.san@gmail.com>
2022-04-09 00:30:40 +0900

    動的にテストをするコード

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,37 +1,25 @@
 const express = require('express');
 const UserService = require('./UserService');
+const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 
-const validateUsername = (req, res, next) => {
-  const user = req.body;
-  if (user.username === null) {
-    req.validationErrors = {
-      username: 'Username cannot be null',
-    };
-  }
-  next();
-};
-
-const validateEmail = (req, res, next) => {
-  const user = req.body;
-  if (user.email === null) {
-    req.validationErrors = {
-      ...req.validationErrors,
-      email: 'Email cannot be null',
-    };
-  }
-  next();
-};
-
 // middleware (validateUsername)
-router.post('/api/1.0/users', validateUsername, validateEmail, async (req, res) => {
-  if (req.validationErrors) {
-    const response = { validationErrors: { ...req.validationErrors } };
-    return res.status(400).send(response);
+router.post(
+  '/api/1.0/users',
+  check('username').notEmpty().withMessage('Username cannot be null'),
+  check('email').notEmpty().withMessage('Email cannot be null'),
+  check('password').notEmpty().withMessage('Password cannot be null'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const validationErrors = {};
+      errors.array().forEach((error) => (validationErrors[error.param] = error.msg));
+      return res.status(400).send({ validationErrors: validationErrors });
+    }
+    await UserService.save(req.body);
+    return res.send({ message: 'User created' });
   }
-  await UserService.save(req.body);
-  return res.send({ message: 'User created' });
-});
+);
 
 module.exports = router;
