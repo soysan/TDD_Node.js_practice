c34c308c8413541ed5c865573d00a0549e7cff39
Masayuki <soy2.san@gmail.com>
2022-04-11 00:02:11 +0900

    条件追加

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const UserService = require('./UserService');
 const { check, validationResult } = require('express-validator');
-
 const router = express.Router();
 
 // middleware (validateUsername)
 router.post(
   '/api/1.0/users',
-  check('username').notEmpty().withMessage('Username cannot be null'),
-  check('email').notEmpty().withMessage('Email cannot be null'),
-  check('password').notEmpty().withMessage('Password cannot be null'),
+  check('username')
+    .notEmpty()
+    .withMessage('Username cannot be null')
+    .bail()
+    .isLength({ min: 4, max: 32 })
+    .withMessage('Must have min 4 and max 32 characters'),
+  check('email')
+    .notEmpty()
+    .withMessage('Email cannot be null')
+    .bail()
+    .isEmail()
+    .withMessage('Email is not valid')
+    .bail()
+    .custom(async (email) => {
+      const user = await UserService.findByEmail(email);
+      if (user) {
+        throw new Error('Email in use');
+      }
+    }),
+  check('password')
+    .notEmpty()
+    .withMessage('Password cannot be null')
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters')
+    .bail()
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)
+    .withMessage('Password must have at least 1 uppercase, 1 lowercase letter and 1 number'),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
